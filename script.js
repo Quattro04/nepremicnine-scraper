d87733fb393cf10f429d5e4d26573c02d0708b6a
Matija Jeras <matija.jeras64@gmail.com>
2022-10-08 14:37:25 +0200

    implemented firebase

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ import fs from 'fs';
 // import { initializeApp } from 'firebase/app';
 // import { getAuth, signInWithCustomToken } from "firebase/auth";
 import admin from 'firebase-admin';
-import { getFirestore, collection, getDocs, addDoc } from 'firebase/firestore/lite';
+// import { getFirestore, collection, getDocs, addDoc } from 'firebase/firestore/lite';
+// import { doc, setDoc } from "firebase/firestore"; 
+import { collection, addDoc } from "firebase/firestore"; 
 // import firebaseConfig from 'config/config.json';
 import serviceAccount  from './config/auth.json' assert {type: 'json'};
 
@@ -22,100 +24,90 @@ admin.initializeApp({
 
 const db = admin.firestore()
 
-try
-{
-   await db.collection("items")
-      .set("123141")
-      .update({
-         "id": "123141",
-         "title": "BEST HOUSE",
-         "url": "www",
-         "region": "neki"
-      })
-}
-catch(err)
-{
-   console.log(err)
+const getItemsFromWebsite = async (items, newItems, lastItems) => {
+
+    for(let i = 0; i < items.length; i++) {
+        const id = await items[i].getAttribute("id");
+        if (id === lastItems[REGIONS[r]]) break;
+        const titleElement = await items[i].findElement(By.className('title'));
+        const title = await titleElement.getText();
+        const anchorElement = await items[i].findElement(By.xpath(`//a[@title="${id.substring('1')}"]`));
+        const url = await anchorElement.getAttribute('href');
+        const velikostElement = await items[i].findElement(By.className('velikost'));
+        const velikost = await velikostElement.getText();
+        const cenaElement = await items[i].findElement(By.className('cena'));
+        const cena = await cenaElement.getText();
+
+        if (!newItems[REGIONS[r]]) newItems = { ...newItems, [REGIONS[r]]: [] };
+
+        newItems[REGIONS[r]].push({ id, title, url, velikost, cena, region: REGIONS[r], date: new Date().toISOString() });
+    }
 }
  
 const getPosesti = async (driver) => {
-   let rawdata = fs.readFileSync('posestiLast.json');
-   let lastItems = JSON.parse(rawdata);
+    let rawdata = fs.readFileSync('posestiLast.json');
+    let lastItems = JSON.parse(rawdata);
 
-   let newItems = {};
+    let newItems = {};
 
-   for(let r = 0; r < REGIONS.length; r++) {
-      await driver.get(`https://www.nepremicnine.net/oglasi-prodaja/${REGIONS[r]}/posest/zazidljiva/?s=16`);
+    for(let r = 0; r < REGIONS.length; r++) {
+        await driver.get(`https://www.nepremicnine.net/oglasi-prodaja/${REGIONS[r]}/posest/zazidljiva/?s=16`);
 
-      const items = await driver.findElements(By.className('oglas_container'));
+        const items = await driver.findElements(By.className('oglas_container'));
 
-      for(let i = 0; i < items.length; i++) {
-         const id = await items[i].getAttribute("id");
-         if (id === lastItems[REGIONS[r]]) break;
-         const titleElement = await items[i].findElement(By.className('title'));
-         const title = await titleElement.getText();
-         const anchorElement = await items[i].findElement(By.xpath(`//a[@title="${id.substring('1')}"]`));
-         const url = await anchorElement.getAttribute('href');
+        await getItemsFromWebsite(items, newItems, lastItems);
+    }
 
-         if (!newItems[REGIONS[r]]) newItems = { ...newItems, [REGIONS[r]]: [] };
+    try {
+        db.collection("posesti").add(newItems);
+    } catch(error) {
+        console.log(error);
+    };
 
-         newItems[REGIONS[r]].push({ id, title, url, region: REGIONS[r] });
+    fs.writeFileSync("posestiNew.json", JSON.stringify(newItems, null, 4));
 
-         // addDoc(db, { id, title, url, region: REGIONS[r] })
-      }
-   }
+    // Update last posesti
+    for (let i = 0; i < Object.keys(newItems).length; i++) {
+        const region = Object.keys(newItems)[i];
+        lastItems[region] = newItems[region][0].id;
+    }
 
-   fs.writeFileSync("posestiNew.json", JSON.stringify(newItems, null, 4));
+    fs.writeFileSync("posestiLast.json", JSON.stringify(lastItems, null, 4));
 
-   // return Promise.resolve();
-
-   // Update last posesti
-   for (let i = 0; i < Object.keys(newItems).length; i++) {
-      const region = Object.keys(newItems)[i];
-      lastItems[region] = newItems[region][0].id;
-   }
-
-   fs.writeFileSync("posestiLast.json", JSON.stringify(lastItems, null, 4));
-
-   return Promise.resolve();
+    return Promise.resolve();
 }
 
 const getHise = async (driver) => {
-   let rawdata = fs.readFileSync('hiseLast.json');
-   let lastItems = JSON.parse(rawdata);
+    let rawdata = fs.readFileSync('hiseLast.json');
+    let lastItems = JSON.parse(rawdata);
 
-   let newItems = {};
+    let newItems = {};
 
-   for(let r = 0; r < REGIONS.length; r++) {
-      await driver.get(`https://www.nepremicnine.net/oglasi-prodaja/${REGIONS[r]}/hisa/?s=16`);
+    for(let r = 0; r < REGIONS.length; r++) {
+        await driver.get(`https://www.nepremicnine.net/oglasi-prodaja/${REGIONS[r]}/hisa/?s=16`);
 
-      const items = await driver.findElements(By.className('oglas_container'));
+        const items = await driver.findElements(By.className('oglas_container'));
 
-      for(let i = 0; i < items.length; i++) {
-         const id = await items[i].getAttribute("id");
-         if (id === lastItems[REGIONS[r]]) break;
-         const titleElement = await items[i].findElement(By.className('title'));
-         const title = await titleElement.getText();
-         const anchorElement = await items[i].findElement(By.xpath(`//a[@title="${id.substring('1')}"]`));
-         const url = await anchorElement.getAttribute('href');
+        await getItemsFromWebsite(items, newItems, lastItems);
+    }
 
-         if (!newItems[REGIONS[r]]) newItems = { ...newItems, [REGIONS[r]]: [] };
+    try {
+        db.collection("hise").add(newItems);
+    } catch(error) {
+        console.log(error);
+    };
 
-         newItems[REGIONS[r]].push({ id, title, url, region: REGIONS[r] });
-      }
-   }
+    fs.writeFileSync("hiseNew.json", JSON.stringify(newItems, null, 4));
 
-   fs.writeFileSync("hiseNew.json", JSON.stringify(newItems, null, 4));
+    // Update last hise
+    for (let i = 0; i < Object.keys(newItems).length; i++) {
+        const region = Object.keys(newItems)[i];
+        lastItems[region] = newItems[region][0].id;
+    }
 
-   // Update last hise
-   for (let i = 0; i < Object.keys(newItems).length; i++) {
-      const region = Object.keys(newItems)[i];
-      lastItems[region] = newItems[region][0].id;
-   }
+    fs.writeFileSync("hiseLast.json", JSON.stringify(lastItems, null, 4));
 
-   fs.writeFileSync("hiseLast.json", JSON.stringify(lastItems, null, 4));
-
-   return Promise.resolve();
+    return Promise.resolve();
 }
 
 const scrape = async () => {
@@ -128,4 +120,4 @@ const scrape = async () => {
    driver.quit();
 }
 
-// scrape();
+scrape();
